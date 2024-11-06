ddbbe8096e7b2ffb4837ccb8ac00a5069ccb0295
anishamalde <anishamalde95@gmail.com>
2024-11-06 16:37:57 +0000

    Update pacakges

--- App/android/app/src/main/java/com/measureperformance/MainApplication.kt
+++ App/android/app/src/main/java/com/measureperformance/MainApplication.kt
@@ -5,44 +5,28 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.config.ReactFeatureFlags
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-    companion object {
-        const val turboModulesEnabled = true
-        const val fabricEnabled = true
-    }
-    private val mReactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getUseDeveloperSupport() = BuildConfig.DEBUG
+    private val mReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
         override fun getPackages(): List<ReactPackage> {
-            // Packages that cannot be auto-linked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
-            return PackageList(this).packages
+            return PackageList(this).packages.apply {
+                // Add additional packages here if needed
+            }
         }
 
-        override fun getJSMainModuleName() = "index"
-
-        override val isNewArchEnabled: Boolean
-            get() = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean
-            get() = BuildConfig.IS_HERMES_ENABLED
+        override fun getJSMainModuleName(): String = "index"
     }
 
-    override fun getReactNativeHost() = mReactNativeHost
+    override val reactNativeHost: ReactNativeHost
+   yar
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this,  /* native exopackage */false)
-        ReactFeatureFlags.enableTextMeasureCachePerShadowNode = true
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load(turboModulesEnabled, fabricEnabled)
-        }
-        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+        SoLoader.init(this, /* native exopackage */ false)
+        // Additional initialization code if needed
     }
 }
